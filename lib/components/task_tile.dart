1c16e35dd4612eca41af3220d28e1a72f4feb4e6
Anuoluwapo Famakinwa <famakinwamercy@gmail.com>
2022-05-29 14:50:50 -0700

    changes

--- lib/components/task_tile.dart
+++ lib/components/task_tile.dart
@@ -1,24 +1,4 @@
 import 'package:flutter/material.dart';
-class TaskTile extends StatefulWidget {
-  final String title;
-
-  const TaskTile({Key? key, required this.title
-  }) : super(key: key);
-
-  @override
-  _TaskTileState createState() => _TaskTileState();
-}
-
-class _TaskTileState extends State<TaskTile> {
-  bool isChecked = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(title: Text(widget.title,style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,
-        decoration: TextDecoration.lineThrough ),),
-      trailing: TaskCheckBox(),);
-  }
-}
 
 class TaskCheckBox extends StatefulWidget {
   @override
@@ -26,27 +6,18 @@ class TaskCheckBox extends StatefulWidget {
 }
 
 class _TaskCheckBoxState extends State<TaskCheckBox> {
-   bool? isChecked = true;
-   void toggleCheckBox(bool? newValue){
-     setState(() {
-       isChecked = newValue;
-     });
-   }
+   bool? isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return
-    //   Checkbox(
-    //     activeColor: Colors.blueAccent,
-    //     value: isChecked,
-    //      onChanged:(newValue){
-    //   setState((){
-    //      isChecked = newValue;
-    //    });}
-    // );
-    Checkbox(value: isChecked, onChanged: (bool? newValue){
-      toggleCheckBox(newValue);
+    Checkbox(value: isChecked,
+        onChanged: (bool? newValue){
+      setState(() {
+        isChecked = newValue;
+      });
     }
     );
   }
+
 }
--- lib/todoey.dart
+++ lib/todoey.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:todoey_app/components/task_tile.dart';
-import 'package:todoey_app/screens/add_task.dart';
 class Todoey extends StatefulWidget {
   const Todoey({Key? key}) : super(key: key);
 
@@ -9,6 +8,86 @@ class Todoey extends StatefulWidget {
 }
 
 class _TodoeyState extends State<Todoey> {
+  final textController = TextEditingController();
+  List<String> _taskItems = [];
+
+
+  void removeTask(int index){
+    setState(() {
+      _taskItems.removeAt(index);
+    });
+  }
+
+  Widget taskTile(String task, int index, ) {
+
+    return Dismissible(key: ValueKey(_taskItems[index]),
+      onDismissed: (direction){},
+      confirmDismiss: (direction) async{
+      return await showDialog(context: context, builder: (_)=> deleteTask(index));},
+      background:  Container(color: Colors.red,padding: EdgeInsets.only(left: 15),
+      child: Align(child: Icon(Icons.delete,color: Colors.white,),
+        alignment: Alignment.centerLeft,),),
+      child: ListTile(title: Text(
+        task, style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,
+         ),),
+         trailing: TaskCheckBox()
+      ),
+    );
+  }
+  void addTask(String task){
+    setState(() {
+      _taskItems.add(task);
+    });
+
+  }
+  Widget addToTask(){
+    return Container(padding: EdgeInsets.all(20),
+      decoration:
+      BoxDecoration(borderRadius: BorderRadius.only(topRight: Radius.circular(20),
+          topLeft: Radius.circular(20))),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text("Add Task",textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 30,
+                fontWeight: FontWeight.bold,color: Colors.blueAccent),),
+          TextField(controller: textController,
+            autofocus: true,),
+          FlatButton(color: Colors.blueAccent,
+              onPressed: (){
+                addTask(textController.text);
+                Navigator.of(context).pop();
+              }, child: Text("Add",style: TextStyle(color: Colors.white),))
+        ],),
+    );
+  }
+
+
+
+  Widget _buildTaskTile(){
+    return ListView.builder(
+        shrinkWrap: true,itemCount: _taskItems.length,
+        itemBuilder: (context, index){
+        return taskTile(_taskItems[index], index );}
+    );
+  }
+ Widget deleteTask(int index){
+    return AlertDialog(
+      title: Text('Mark "${_taskItems[index]}" as done?'),
+      actions: <Widget>[
+        FlatButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text("Cancel")
+        ),
+        FlatButton(
+          onPressed: () {
+            removeTask(index);
+              Navigator.of(context).pop();},
+          child: const Text("Mark as done"),
+        ),
+      ],
+    );
+ }
+  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(backgroundColor: Colors.blueAccent,
@@ -24,17 +103,16 @@ class _TodoeyState extends State<Todoey> {
             SizedBox(height: 10,),
             Text("Todoey",
               style: TextStyle(fontWeight: FontWeight.bold,fontSize: 40,color: Colors.white),),
-                Text("12 tasks",style: TextStyle(color: Colors.white,fontSize: 15),),],),),
+                Text("${_taskItems.length} tasks",style: TextStyle(color: Colors.white,fontSize: 15,
+                ),),],),),
           Expanded(
             child: Container(height: 300,
               decoration: BoxDecoration(color: Colors.white,borderRadius:
               BorderRadius.only(topLeft: Radius.circular(20),
                   topRight: Radius.circular(20))),
               child: Column(
-                children: [SizedBox(height: 15,),
-                  TaskTile(title: 'Buy bread',),
-                  TaskTile(title: 'Buy milk',),
-                  TaskTile(title: 'Buy egg',),
+                children: [
+                _buildTaskTile()
                 ],
               )
 
@@ -45,9 +123,11 @@ class _TodoeyState extends State<Todoey> {
     floatingActionButton: FloatingActionButton(backgroundColor: Colors.blueAccent,
       child: Icon(Icons.add),
       onPressed: (){
-      showModalBottomSheet(context: context, builder: (context)=>AddTask());
+      showModalBottomSheet(
+          context: context, builder: (context)=>addToTask());
       },),);
   }
+
 }
 
 
